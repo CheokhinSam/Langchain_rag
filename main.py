9138ac9a8ebeff77d40ae5c4b28cbfe8645dd2b6
samchan <cheokhinchan@gmail.com>
2025-02-11 22:52:38 +0800

    add model

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import getpass
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 if not os.environ.get("AZURE_OPENAI_API_KEY"):
   os.environ["AZURE_OPENAI_API_KEY"] = getpass.getpass("Enter API key for Azure: ")
 
+#llm model
 from langchain_openai import AzureChatOpenAI
-
 llm = AzureChatOpenAI(
     azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
     azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT_NAME"],
     openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
 )
+
+#Embedding model
+from langchain_openai import AzureOpenAIEmbeddings
+
+embeddings = AzureOpenAIEmbeddings(
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT_NAME"],
+    openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+)
+
+#vector store
+from langchain_chroma import Chroma
+vector_store = Chroma(embedding_function=embeddings)
+
